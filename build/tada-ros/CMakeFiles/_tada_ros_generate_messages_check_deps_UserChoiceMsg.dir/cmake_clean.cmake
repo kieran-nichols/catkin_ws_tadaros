82911cf471b2fc7f4dde29f986867b8b3a11db17
kieran-nichols <the1kieran@gmail.com>
2022-10-17 16:02:15 -0500

    completed a basic commenting session for the motor code

--- build/tada-ros/CMakeFiles/_tada_ros_generate_messages_check_deps_UserChoiceMsg.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/_tada_ros_generate_messages_check_deps_UserChoiceMsg"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/_tada_ros_generate_messages_check_deps_UserChoiceMsg.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
