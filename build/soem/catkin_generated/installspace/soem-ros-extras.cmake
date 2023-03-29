a5d76291d690686e5c111d0c94c7fde3036f4ef7
Sofya Akhetova <71956317+akhetovasofya@users.noreply.github.com>
2023-03-28 23:26:23 -0500

    Delete build directory

--- build/soem/catkin_generated/installspace/soem-ros-extras.cmake
+++
@@ -1,2 +0,0 @@
-list(APPEND x_soem_INCLUDE_DIRS "${x_soem_PREFIX}/include")
-list(APPEND x_soem_INCLUDE_DIRS "${x_soem_PREFIX}/include/soem")
--- build/tada-ros/catkin_generated/setup_py_interrogation.cmake
+++
@@ -1,7 +0,0 @@
-set(tada_ros_SETUP_PY_SETUP_MODULE "distutils.core")
-set(tada_ros_SETUP_PY_VERSION "0.0.0")
-set(tada_ros_SETUP_PY_SCRIPTS "")
-set(tada_ros_SETUP_PY_PACKAGES "tada_ros")
-set(tada_ros_SETUP_PY_PACKAGE_DIRS "src/tada_ros")
-set(tada_ros_SETUP_PY_MODULES "")
-set(tada_ros_SETUP_PY_MODULE_DIRS "")
