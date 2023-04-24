bd6ec4dbad1ac8619515c1ad92f45ffbb8f4a901
Sofya Akhetova <71956317+akhetovasofya@users.noreply.github.com>
2023-04-23 21:48:10 -0500

    Delete build directory

--- build/soem/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/catkin_ws/src/soem
-# Build directory: /home/pi/catkin_ws/build/soem
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("SOEM")
