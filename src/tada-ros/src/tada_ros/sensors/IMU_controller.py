2e3f41929cd1824dbb8b1b21be6dcd4bcf055853
Sofya Akhetova <71956317+akhetovasofya@users.noreply.github.com>
2023-08-19 11:36:32 -0500

    Update IMU_controller.py

--- src/tada-ros/src/tada_ros/sensors/IMU_controller.py
+++ src/tada-ros/src/tada_ros/sensors/IMU_controller.py
@@ -3,6 +3,8 @@
 # Much of this is from Interfacing Raspberry Pi with MPU6050
 # http://www.electronicwings.com
 
+#This file is mostly to establish functions for sensor_node.py to call on
+
 import smbus # SMBus module of I2C
 #pypi.org/project/mpu6050-raspberrypi
 from mpu6050 import mpu6050
@@ -22,7 +24,6 @@ import rospy
 DEBUG_FLAG = 0
 linear_correction = 1
 
-# todo: what of this should be in constants file?
 #some MPU6050 Registers and their Address
 PWR_MGMT_1   = 0x6B
 SMPLRT_DIV   = 0x19
@@ -47,22 +48,27 @@ class Triple():
 
 class IMUData():
     def __init__(self, accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z, state, swing_time, t, steps):
+        #this just formats the data for ROS message submission
         self.accel = Triple(accel_x, accel_y, accel_z)
         self.gyro = Triple(gyro_x, gyro_y, gyro_z)
         self.swing = Triple(state, swing_time, t)
         self.data = Triple(steps, 0, 0)
 
     def accel_magnitude(self):
+        #gives the magnitude for acceleration
         return numpy.linalg.norm([self.accel.x, self.accel.y, self.accel.z])
 
     def gyro_magnitude(self):
+        #gives the magnitude for gyroscope
         return numpy.linalg.norm([self.gyro.x, self.gyro.y, self.gyro.z])
 
     def to_ROS_message(self):
+        #formats into msg
         return IMUDataMsg(self.accel.x, self.accel.y, self.accel.z, \
                 self.gyro.x, self.gyro.y, self.gyro.z, self.swing.x, self.swing.y , self.swing.z, self.data.x)
 
     def to_string(self):
+        #puts all the values into string
         str = "accel_x: %.6f; accel_y: %.6f; accel_z: %.6f;\n" \
             % (self.accel.x, self.accel.y, self.accel.z)
         str += "gyro_x: %.6f; gyro_y: %.6f; gyro_z: %.6f;\n" \
@@ -72,6 +78,7 @@ class IMUData():
         return str
 
     def print(self):
+        #prints all the values
         print("accel_x: %.6f; accel_y: %.6f; accel_z: %.6f; " \
             % (self.accel.x, self.accel.y, self.accel.z))
         print("gyro_x: %.6f; gyro_y: %.6f; gyro_z: %.6f; " \
@@ -80,20 +87,19 @@ class IMUData():
             % (self.swing.x, self.swing.y, self.swing.z))
 
 def ROS_message_to_IMUData(msg_data):
-    #print("IMU message function")
+    #formats into msg
     return IMUData(msg_data.accel_x, msg_data.accel_y, msg_data.accel_z, \
                     msg_data.gyro_x, msg_data.gyro_y, msg_data.gyro_z, msg_data.state, msg_data.swing_time, msg_data.t, msg_data.steps)
 
 class IMUController():
-    # initialize class variables
-    # bus for I2C
-    cur_time = time.time()
+    # initialize class variables for I2C bus
+    cur_time = time.time() #gets current time
     measured_dt = constants.DT
     Device_Address = 0x68   # MPU6050 device address
-    sensor = mpu6050(Device_Address)
+    sensor = mpu6050(Device_Address) #gets the device
     ## initialization of IMU
     def __init__(self):
-
+        #initializing all initial values just once
         self.state = 0
         self.start = time.time()
         self.start_time = time.time()
@@ -107,7 +113,7 @@ class IMUController():
         self.swing_time=0
         self.steps = 0
 
-    ## collect continuous steam of IMU accel and gyro data and output de-biased data
+    ## Collect continuous steam of IMU accel and gyro data and output de-biased data
     def get_data(self):
         
         # Adjust raw data for scale factor
@@ -119,32 +125,31 @@ class IMUController():
         gyro_y = self.sensor.get_gyro_data().get('y')
         gyro_z = self.sensor.get_gyro_data().get('z')
 
-        #SWING
+        #Detectes whether we are in swing or stance based on gyro
         if gyro_z > self.gyro_thres: # swing
             self.state = 1
             # collect the swing time and save the data only once
             if self.initial_itr == 0:
                 self.start_time = time.time()
-                #print(self.avg_val_swing)
                 self.initial_itr = 1
+                
         # when not saving data, move the motor at the first itr
         # and collect the swing time
-        else: # stance
+        else: #stance
             if self.initial_itr ==1:
-                
-                self.swing_time = time.time() - self.start_time
-                if self.swing_time > 0.2:
-                    self.steps+=1
-                    self.swing.append(self.swing_time)
-                    avg_swing = self.swing[-3:]
-                    self.avg_val_swing = np.mean(avg_swing)
+                self.swing_time = time.time() - self.start_time #collecting elapsed swing time
+                if self.swing_time > 0.2: #to cut off noise from having steps that are impossibly small
+                    self.steps+=1 #incrementing how many steps
+                    self.swing.append(self.swing_time) #appending new swing time
+                    avg_swing = self.swing[-3:] #getting the last 3 steps
+                    self.avg_val_swing = np.mean(avg_swing) #averaging the swing time of the last 3 steps
                 # limit the avg swing time to be at minimum 0.2
-                
             self.initial_itr = 0
             self.state = 0
         
-        current_time = rospy.Time.now()
-        current_time_value = current_time.to_sec()
-        current_time_value = current_time_value%100000
+        current_time = rospy.Time.now() #get current time
+        current_time_value = current_time.to_sec() #cutting off time for formatting and reporting purposes
+        current_time_value = current_time_value%100000 #cutting off time for formatting and reporting purposes
+        #putting all this data into a message object
         imu_data = IMUData(accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z, self.state, self.avg_val_swing, current_time_value, self.steps)
         return imu_data
